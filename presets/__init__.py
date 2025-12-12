1fb35a8ab63b7d900e5f0eed2c60097d31e472f7
Yongce Liu <airrighttime@outlook.com>
2025-12-12 10:26:39 +0800

    [remove] ununsed configs

--- presets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Robot presets and configurations."""
-
-TYPE_CHECKING
-
-if TYPE_CHECKING:
-    pass
-
-__all__ = []
--- presets/unitree/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Unitree robot presets."""
-
-TYPE_CHECKING
-
-if TYPE_CHECKING:
-    pass
-
-__all__ = []
